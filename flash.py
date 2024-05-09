dcae89ce25b0f645bb5ad9618eccb630250e9d57
m24h <jjkkxyz@hotmail.com>
2024-05-10 04:01:46 +0800

    Added support for ESP32S2 Signed-off-by: m24h <jjkkxyz@hotmail.com>

--- flash.py
+++ flash.py
@@ -1,47 +1,87 @@
+# NOTICE:
+#   not all MCU support mem8()/mem16() method on FLASH,
+#   with those MCU, data must be read in machine word using mem32(), or panic
+
 import sys
 import machine
+from micropython import const
 from array import array
 from struct import unpack
 
-if 'esp32c3' in sys.implementation._machine.lower():
-    import esp
-    import esp32
+import esp
+import esp32
+size=esp.flash_size()
+from esp import flash_read  as readinto
+from esp import flash_write as write
+from esp import flash_erase as erase
+
+soc=sys.implementation._machine.lower()
+if 'c3' in soc:
+    chip='ESP32C3'
     page=4096
-    size=esp.flash_size()
-    from esp import flash_read  as readinto
-    from esp import flash_write as write
-    from esp import flash_erase as erase
-    # make all flash MMU mapped, for ESP32C3, there are 128 entries, each enter means 64k,
+    # make all flash MMU mapped, for ESP32C3, there are 128 entries, each entry means 64k,
     # and virtual address is from 0x3c000000, MMU table is from 0x600C5000
     _mmu=array('L', (0,)*128)
     free=array('L')
     for i in range(128):
         t=machine.mem32[0x600C5000+(i<<2)]
-        if t&0x100:
+        if t&0x100: # invalid bit
             free.append(i)
-        elif not t&~0x7F:
-            _mmu[t]=0x3c000000+(i<<16)
+        else:
+            t=t&0xFF
+            if t<128: # only support 0-8M 
+                _mmu[t]=0x3c000000+(i<<16)
     t=0
     for i in range(size>>16):
         if not _mmu[i]:
             if t>=len(free):
                 raise MemoryError('No available MMU entry')
             machine.mem32[0x600C5000+(free[t]<<2)]=i
-            _mmu[i]=0x3c000000+(free[t]<<16)
-            t=t+1
-    def vaddr(faddr):
-        return _mmu[faddr>>16]+(faddr&0xffff)
-    # find a partition which has a label 'bin'
-    if t:=esp32.Partition.find(type=esp32.Partition.TYPE_DATA, label='bin'):
-        t=t[0].info()
-        bin_addr=t[2]
-        bin_size=t[3]
-    else:
-        bin_addr=0
-        bin_size=0
+            _mmu[i]=0x3C000000+(free[t]<<16)
+            t+=1
     del t, i, free
+elif 's2' in soc:
+    chip='ESP32S2'
+    page=4096
+    # make all flash MMU mapped, for ESP32S2, there are 6*64 entries, each entry means 64k,
+    # we now only use first 3*64-8 IBUS entries, which's indexes are from 8 to 191
+    # and virtual address is from 0x40000000 to 0x407FFFFF, MMU table is from 0x61801000
+    _mmu=array('L', (0,)*128)
+    free=array('L')
+    for i in range(192):
+        t=machine.mem32[0x61801000+(i<<2)]
+        if t&0x4000: # invalid bit
+            if i>=8:
+                free.append(i)
+        elif t&0x8000: # flash bit
+            t=t&0x3FFF
+            if t<128: # virtual address support 0-8M
+                _mmu[t]=0x40000000+(i<<16)
+    t=0
+    for i in range(size>>16):
+        if not _mmu[i]:
+            if t>=len(free):
+                raise MemoryError('No available MMU entry')
+            machine.mem32[0x61801000+(free[t]<<2)]=i|0x8000 # FLASH flag
+            _mmu[i]=0x40000000+(free[t]<<16)
+            t+=1
+    del t, i, free
+   
 else:
     raise NotImplementedError('Not a supported MCU')
+
+def vaddr(faddr):
+    return _mmu[faddr>>16]+(faddr&0xffff)
+
+# find a partition which has a label 'bin'
+if t:=esp32.Partition.find(type=esp32.Partition.TYPE_DATA, label='bin'):
+    t=t[0].info()
+    bin_addr=t[2]
+    bin_size=t[3]
+else:
+    bin_addr=0
+    bin_size=0
+del t
 
 def mem32(pos):
     return machine.mem32[vaddr(pos)]
