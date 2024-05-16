698bf9f5d72aa9135b9506ec4c53ff97989f6764
m24h <jjkkxyz@hotmail.com>
2024-05-17 02:27:56 +0800

    clean up some imported modules Signed-off-by: m24h <jjkkxyz@hotmail.com>

--- flash.py
+++ flash.py
@@ -2,29 +2,24 @@
 #   not all MCU support mem8()/mem16() method on FLASH,
 #   with those MCU, data must be read in machine word using mem32(), or panic
 
-import sys
-import machine
-from micropython import const
-from array import array
-from struct import unpack
+from machine import mem32 as _mm32, mem16 as _mm16, mem8 as _mm8
 
-import esp
-import esp32
-size=esp.flash_size()
+size=__import__('esp').flash_size()
+
 from esp import flash_read  as readinto
 from esp import flash_write as write
 from esp import flash_erase as erase
 
-soc=sys.implementation._machine.lower()
+soc=__import__('sys').implementation._machine.lower()
 if 'c3' in soc:
     chip='ESP32C3'
     page=4096
     # make all flash MMU mapped, for ESP32C3, there are 128 entries, each entry means 64k,
     # and virtual address is from 0x3c000000, MMU table is from 0x600C5000
-    _mmu=array('L', (0,)*128)
-    free=array('L')
+    _mmu=__import__('array').array('L', (0,)*128)
+    free=__import__('array').array('L')
     for i in range(128):
-        t=machine.mem32[0x600C5000+(i<<2)]
+        t=_mm32[0x600C5000+(i<<2)]
         if t&0x100: # invalid bit
             free.append(i)
         else:
@@ -36,7 +31,7 @@ if 'c3' in soc:
         if not _mmu[i]:
             if t>=len(free):
                 raise MemoryError('No available MMU entry')
-            machine.mem32[0x600C5000+(free[t]<<2)]=i
+            _mm32[0x600C5000+(free[t]<<2)]=i
             _mmu[i]=0x3C000000+(free[t]<<16)
             t+=1
     del t, i, free
@@ -46,10 +41,10 @@ elif 's2' in soc:
     # make all flash MMU mapped, for ESP32S2, there are 6*64 entries, each entry means 64k,
     # we now only use first 3*64-8 IBUS entries, which's indexes are from 8 to 191
     # and virtual address is from 0x40000000 to 0x407FFFFF, MMU table is from 0x61801000
-    _mmu=array('L', (0,)*128)
-    free=array('L')
+    _mmu=__import__('array').array('L', (0,)*128)
+    free=__import__('array').array('L')
     for i in range(192):
-        t=machine.mem32[0x61801000+(i<<2)]
+        t=_mm32[0x61801000+(i<<2)]
         if t&0x4000: # invalid bit
             if i>=8:
                 free.append(i)
@@ -62,18 +57,19 @@ elif 's2' in soc:
         if not _mmu[i]:
             if t>=len(free):
                 raise MemoryError('No available MMU entry')
-            machine.mem32[0x61801000+(free[t]<<2)]=i|0x8000 # FLASH flag
+            _mm32[0x61801000+(free[t]<<2)]=i|0x8000 # FLASH flag
             _mmu[i]=0x40000000+(free[t]<<16)
             t+=1
     del t, i, free
-   
 else:
     raise NotImplementedError('Not a supported MCU')
+del soc
 
 def vaddr(faddr):
     return _mmu[faddr>>16]+(faddr&0xffff)
 
 # find a partition which has a label 'bin'
+import esp32
 if t:=esp32.Partition.find(type=esp32.Partition.TYPE_DATA, label='bin'):
     t=t[0].info()
     bin_addr=t[2]
@@ -81,16 +77,16 @@ if t:=esp32.Partition.find(type=esp32.Partition.TYPE_DATA, label='bin'):
 else:
     bin_addr=0
     bin_size=0
-del t
+del t, esp32
 
 def mem32(pos):
-    return machine.mem32[vaddr(pos)]
+    return _mm32[vaddr(pos)]
 
 def mem16(pos):
-    return machine.mem16[vaddr(pos)]
+    return _mm16[vaddr(pos)]
 
 def mem8(pos):
-    return machine.mem8[vaddr(pos)]
+    return _mm8[vaddr(pos)]
 
 class Bin:
     def __init__(self, addr, size):
@@ -98,19 +94,21 @@ class Bin:
         self.size=size
     
     def mem32(self, pos):
-        return machine.mem32[vaddr(pos+self.addr)]
+        return _mm32[vaddr(pos+self.addr)]
     
     def mem16(self, pos):
-        return machine.mem16[vaddr(pos+self.addr)]
+        return _mm16[vaddr(pos+self.addr)]
     
     def mem8(self, pos):
-        return machine.mem8[vaddr(pos+self.addr)]
+        return _mm8[vaddr(pos+self.addr)]
     
     def readinto(self, offset, barray):
         readinto(offset+self.addr, barray)
     
+    # several files can be packed in to a Bin object, see bin_pack.py/bin_unpack.py
     # encoded label must be no longer than 8 bytes
     def findsub(self, label):
+        from struct import unpack
         idx=0
         b=bytearray(16)
         label=label.encode('utf-8')
